810c2986fe7e40f541336dd9e7520aacf070ffb1
Angelina <panteley-00@mail.ru>
2017-09-24 11:36:27 +0300

    my solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,36 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.maxIndex=-1;
+        this.minIndex=-1;
+        this.guessIndex=-1;
+        this.range=[];
+        this.length=0;
+    }
 
     setRange(min, max) {
-
+        this.length=max-min+1;
+        this.range=new Array(this.length);
+        for(var i=0;i<this.length;i++) {
+            this.range[i]=min+i;
+        }
+        this.minIndex=0;
+        this.maxIndex=this.length-1;
     }
 
     guess() {
-
+        if((this.minIndex+this.maxIndex)%2===0)
+            this.guessIndex=(this.minIndex+this.maxIndex)/2;
+        else
+            this.guessIndex=(this.minIndex+this.maxIndex+1)/2;
+        return this.range[this.guessIndex];
     }
 
     lower() {
-
+        this.maxIndex=this.guessIndex;
     }
 
     greater() {
-
+        this.minIndex=this.guessIndex;
     }
 }
 
